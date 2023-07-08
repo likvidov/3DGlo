7101ed83de77e5ece5520c0b0672532892824c89
likvidov <likvidya@gmail.comm>
2023-07-08 17:31:55 +0300

    Написал скрипт по видео

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import timer from './modules/timer';
+import menu from './modules/menu';
+import modal from './modules/modal';
 
 timer('11 jule 2023');
+menu();
+modal();
--- src/modules/two.js
+++
@@ -1,5 +0,0 @@
-const two = () => {
-  console.log('Two');
-}
-
-export default two;
