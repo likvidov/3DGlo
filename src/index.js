5d06d39341dc98a94c7ab59c9362ae5dd2b04893
likvidov <likvidya@gmail.comm>
2023-07-10 18:35:03 +0300

    Выполненое задание 21 урока

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
+import validator from './modules/validator';
 
 timer('11 jule 2023');
 menu();
 modal();
+validator();
