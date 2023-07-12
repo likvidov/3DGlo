4b84cf0ddec052d366382433591bad1cd711b1e2
likvidov <likvidya@gmail.comm>
2023-07-12 14:49:48 +0300

    Реализован калькулятор

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import validator from './modules/validator';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calc from './modules/calc';
 
 timer('14 jule 2023');
 menu();
@@ -11,3 +12,4 @@ modal();
 validator();
 tabs();
 slider('.portfolio-content', '.portfolio-item', '.portfolio-dots');
+calc();
